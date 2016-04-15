8179068f8ae68530cb98af9b1bb6ec04843042ef
Donovan Graham <donovan@platform7.com>
2016-04-15 20:03:40 +0200

    Adding pretender whilst offline. POC. Need to do this via npm and not bower

--- app/index.jsx
+++ app/index.jsx
@@ -15,6 +15,35 @@ import routes from 'routes';
 const store = configureStore();
 const history = syncHistoryWithStore(browserHistory, store);
 
+// debugger;
+
+const fakeServer = new Pretender();
+window.fakeServer = fakeServer;
+
+fakeServer.get('https://api.github.com', () => {
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+  const response = {
+    current_user_url: 'Yo Donny Gee',
+    username: 'Yo yo yo',
+  };
+  return [200, headers, JSON.stringify(response)];
+});
+
+fakeServer.handledRequest = (verb, path) => {
+  const _verb = verb.toLowerCase();
+  console.log(`** AJAX ${_verb} ${path}`);
+};
+
+fakeServer.unhandledRequest = (verb, path, request) => {
+  console.log(`Eh? unhandled request: ${verb} ${path}`, request);
+};
+
+
+
+
+
 ReactDOM.render(
   <Provider store={store}>
     <Router history={history} routes={routes} />
